7bd3857465c07d5b880d4aaae67682e6c822842f
Aluno <aluno@decom-OptiPlex-780>
2017-05-05 17:16:08 -0300

    Adiciona TAD Pilha Lista Encadeada

--- Tp 2/TAD_Pilha_Lista_Encadeada/TAD.cpp
+++ Tp 2/TAD_Pilha_Lista_Encadeada/TAD.cpp
@@ -16,12 +16,16 @@ struct pilha
 	celula *fim;
 };
 
+bool Pilha_vazia(pilha *p){
+	return (*p).fim==NULL;
+}
+
 void Empilha(pilha *p, int num, char *str){
 	celula *cel = (celula *) malloc(sizeof(celula));
 	(*cel).valor=num;
 	strncpy((*cel).caracteres, str, 30);
 	(*cel).prox = NULL;
-	if((*p).fim==NULL){
+	if(Pilha_vazia(p)){
 		(*cel).id=1;
 		(*p).fim=cel;			
 	}
@@ -32,13 +36,63 @@ void Empilha(pilha *p, int num, char *str){
 	}
 }
 
+void Desempilha(pilha *p){
+	if(Pilha_vazia(p))
+		cout<<"Pilha vazia"<<endl;
+	else{
+		celula *aux = (*p).fim->prox;
+		free((*p).fim);
+		(*p).fim = aux;
+	}
+}
+
+void Printa_topo(pilha *p){
+	if(Pilha_vazia(p))
+		cout<<"Pilha vazia"<<endl;
+	else{
+		cout<<(*p).fim->valor<<" "<<(*p).fim->caracteres<<endl;
+	}
+}
+
+void Tamanho(pilha *p){
+	if(Pilha_vazia(p))
+		cout<<"0"<<endl;
+	else
+		cout<<(*p).fim->id<<endl;
+}
+
+void Esvazia_pilha(pilha *p){
+	int t = (*p).fim->id;
+	for(int i=0; i<t; i++)
+		Desempilha(p);
+}
+
 int main(){
+	int num;
+	char cha[30], ch;
 	pilha p;
 	p.fim=NULL;
-	cout<<"oi\n";
-	
-	Empilha(&p, 10, (char *)"os");
-	Empilha(&p, 12, (char *)"os");
-	cout<<"Olha = "<<p.fim->valor<<endl;
+
+	while(!cin.eof()){
+		cin>>ch;
+		switch(ch){
+			case 'E':
+				cin>>num;
+				cin>>cha;
+				Empilha(&p, num, cha);
+				break;
+			case 'D':
+				Desempilha(&p);
+				break;
+			case 'T':
+				Tamanho(&p);
+				break;
+			case 'I':
+				Printa_topo(&p);
+				break;
+			case 'S':
+				return 0;
+		}
+	}
 	return 0;
 }
